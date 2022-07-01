64ee3f4dfacff73c6f8cb98652001db58820a976
Panthi <anish.panthi41@gmail.com>
2022-07-01 14:44:43 -0500

    Initial Commit

---
+++ src/main/java/com/app/reactive/ReactiveRabbitmqAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRabbitmqAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRabbitmqAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/reactive/ReactiveRabbitmqAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRabbitmqAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
